2e3d57d1937531fedb03e1636774282916878db5
root <root@LAPTOP-IPDNQQNN.localdomain>
2022-03-20 01:03:18 -0300

    Lista de Exercicíos 02

---
+++ lista_de_exercicios_02/exercicio-02.py
@@ -0,0 +1,54 @@
+# Exercício 02.
+
+import math
+
+5/2
+# Resultado = 2.5
+
+7 * 4 + 2
+# Resultado = 30
+
+(7 * 4) + 2
+# Resultado = 30
+
+7 * (4 + 2)
+# Resultado = 42
+
+2 ** 3
+# Resultado = 8
+
+2 ** 3 ** 4
+# Resultado = 2417851639229258349412352
+
+2 ** -3 ** 4
+# Resultado = 4.1359030627651384e-25
+
+5 % 2
+# Resultado = 1
+
+6 % 2
+# Resultado = 0
+
+7 % 2
+# Resultado = 1
+
+8 % 2
+# Resultado = 0
+
+5 + 1
+# Resultado = 6
+
+5.0 + 1
+# Resultado = 6.0
+
+5 * math.log10(100) - 8 ** 2
+# Resultado = -54.0
+
+math.pi
+# Resultado = 3.141592653589793
+
+math.sin(math.pi / 2 )
+# Resultado = 1.0
+
+math.cos(math.pi / 4 )
+# Resultado = 0.7071067811865476
