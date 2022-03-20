d25679965be82306c758a6b3a979ddf79836b1e8
nildson <nildson.silva@agrosatelite.com.br>
2022-03-19 21:52:55 -0300

    Adicionando uma função para cálculo do fatorial

---
+++ fatorial.py
@@ -0,0 +1,43 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "b31f808c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def Fatorial(num):\n",
+    "    produto = 1\n",
+    "\n",
+    "    while(num > 0):\n",
+    "        produto = produto * num\n",
+    "\n",
+    "        num = num - 1\n",
+    "\n",
+    "    return produto"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
